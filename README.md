6d89ef1f62f61b79990635b2a49ef5a65ae27d0a
Sławomir Zborowski <slawek@slawomir.net>
2022-10-01 01:08:19 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cypress-websocket-plugin
-Cypress plugin for mocking websockets (socket.io version 2)
+
+> Cypress plugin for mocking websockets (socket.io version 2)
+
+WARNING: This is experimental software. It works and was tested against socket.io version 2 only.
+
+Once it's battle-tested in one of my projects I will document it.
---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+function substituteVariables(ref, variables) {
+    if (!ref.startsWith("$")) {
+        return ref;
+    }
+
+    const varName = ref.slice(1);
+    if (!(varName in variables)) {
+        return ref;
+    }
+
+    return variables[varName];
+}
+
+
+function startFakeWebsocketServer() {
+    const app = express();
+    const port = 3000;
+    const server = app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+
+    const io = require('socket.io').listen(server);
+    app.use(cors({ origin: '*' }));
+    app.use(bodyParser.json());
+
+    let messages = [];
+    let variables = [];
+    let timers = [];
+    let options = {
+        responses: {},
+        periodicMessages: [],
+    };
+
+    function setup(socket) {
+        console.log("Setting up new socket with opts:", options);
+
+        Object.keys(options.responses).forEach(function(key) {
+            socket.on(key, () => {
+                const [command, ...params] = options.responses[key]
+                socket.emit(command, ...params);
+            });
+        });
+
+        options.periodicMessages.forEach(v => {
+            timers.push(setInterval(
+                () => {
+                    socket.emit(v.name, ...(v.args.map(x => substituteVariables(x, variables))));
+                },
+                v.interval,
+            ));
+        });
+    }
+
+    function cleanup() {
+        timers.map(clearInterval);
+        timers = [];
+    }
+
+    app.get('/__cypress/messages', (req, res) => {
+        res.json(messages);
+    });
+
+    app.delete('/__cypress/messages', (req, res) => {
+        messages = [];
+        res.json();
+    });
+
+    app.get('/__cypress/options', (req, res) => {
+        res.json(options);
+    });
+
+    app.post('/__cypress/options', (req, res) => {
+        options = req.body;
+        res.json();
+        cleanup();
+    });
+
+    app.get('/__cypress/variables', (req, res) => {
+        res.json(variables);
+    });
+
+    app.post('/__cypress/variables', (req, res) => {
+        variables[req.params.name] = req.params.value;
+        res.json();
+    });
+
+    io.on('connection', socket => {
+        var onevent = socket.onevent;
+        socket.onevent = function (packet) {
+            var args = packet.data || [];
+            onevent.call(this, packet);
+            packet.data = ["*"].concat(args);
+            const name = args[0];
+            if (!name.startsWith("__cypress")) {
+                messages.push([...args]);
+            }
+        };
+
+        setup(socket);
+    });
+}
+
+module.exports = {
+    startFakeWebsocketServer,
+};
